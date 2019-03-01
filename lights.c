ef018cc8a3c59c0ccf5fcc66453121585e5544d0
Dellaviola <41549231+Dellaviola@users.noreply.github.com>
2019-03-01 10:21:34 -0800

    Add files via upload

--- lights.c
+++ lights.c
@@ -9,9 +9,8 @@ high_last = 0b00000010;
 mid_first = 0b10000000;
 mid_tl = 0b01110000;
 mid_last = 0b00000010;
-
 mid_low = 0b00000111;
-mid_jump = 0b00001000;
+
 
 green_tl = 0b00110000;
 yellow_tl = 0b01010000;
@@ -27,7 +26,7 @@ while (1)
 		red_tick_mask = 0;
 		red_tck = 0;
 		low = low>>1;					//move low over
-		low |= (mid & mid_last);		//add the first bit in
+		low |= ((mid & mid_last) << 6);		//add the first bit in
 
 		
 		mid_temp = (mid & mid high);
@@ -44,7 +43,7 @@ while (1)
 	{
 
 		low = low>>1;					//move low over
-		low |= (mid & mid_last);		//add the first bit in
+		low |= ((mid & mid_last) << 6);		//add the first bit in
 
 		
 		mid_temp = (mid & mid high);
@@ -61,7 +60,7 @@ while (1)
 	{
 
 		low = low>>1;					//move low over
-		low |= (mid & mid_last);		//add the first bit in
+		low |= ((mid & mid_last) << 6);		//add the first bit in
 
 		
 		mid = mid >> 1;					//move mid over
@@ -69,7 +68,7 @@ while (1)
 		
 		mid |= (1<<4);					//light immediately after traffic light is off
 		
-		if (!red_tick) mid |= (((high & high_last) << 6)); //if we arent building up traffic move last bit of high over
+		if (!red_tick) mid |= ((high & high_last) << 6); //if we arent building up traffic move last bit of high over
 		
 		mid |= red_tl;									//update the traffic light colour
 		
@@ -85,6 +84,6 @@ while (1)
 			red_tick_mask *= 2;
 		}
 		
-		high &= (~(red_tick - 1) << 1);					//reset the bits in high byte that should have cars building
-}
+		high &= (~(red_tick - 1) << 1);					//reset the bits in high byte that should have cars building up
+	}
 }
