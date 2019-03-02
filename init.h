699113e0ae276bd93c127ccdc6de83d790eb221a
Dellaviola <41549231+Dellaviola@users.noreply.github.com>
2019-03-01 16:02:54 -0800

    comments on init and added notes

--- init.h
+++ init.h
@@ -1,13 +1,34 @@
+
+#ifndef _INIT_H_
+#define  _INIT_H_
 /* Includes */
 #include "stm32f4xx.h"
 
 // Initialize GPIO and ADC and SPI
 void GPIO_ADC_Init(void);
+/*
+    Setup GPIOC ports for ADC
+*/
 
 void GPIOB_Init(void);
+/*
+    Setup GPIOB ports for SPI
+*/
 
 void SPI1_Init(void);
+/*
+    Initialize the SPI to only transmit LSB first
+*/
 
 uint32_t Get_ADC_Value(void);
+/*
+    This function returns the ADC value with 12b precision (0 to 4095)
+*/
 
 void SPI1_Write(uint16_t data);
+/*
+    This function ensures the SPI bus is not busy
+    Then calls the SPI driver functions to write 8 bits
+*/
+
+#endif
